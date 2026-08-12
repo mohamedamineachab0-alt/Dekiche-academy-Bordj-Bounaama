a430d8b495963765d4c9d6fc9483b30f8cfbf097
istore <istore@MacBook-Air-de-istore.local>
2026-08-12 23:24:36 +0100

    Remove scratch scripts to fix production build

--- check-admin.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function main() {
-  const user = await prisma.user.findFirst({
-    where: { phoneNumber: "0562388085" }
-  });
-  console.log(user);
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
